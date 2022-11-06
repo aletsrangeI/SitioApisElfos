6139aca57b7b9200eca5ed53a33e45f594363394
Alecs <Alecs@DESKTOP-5H0J46U>
2022-11-05 22:51:13 -0600

    Carga inicial

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const toast = document.querySelector("#toast");
+const boton = document.querySelector("#boton");
+
+document.addEventListener("DOMContentLoaded", () => {});
+
+boton.addEventListener("click", () => {
+  var toastElList = [].slice.call(document.querySelectorAll(".toast"));
+  var toastList = toastElList.map(function (toastEl) {
+    // Creates an array of toasts (it only initializes them)
+    return new bootstrap.Toast(toastEl); // No need for options; use the default options
+  });
+  toastList.forEach((toast) => toast.show()); // This show them
+
+  console.log(toastList); // Testing to see if it works
+});
